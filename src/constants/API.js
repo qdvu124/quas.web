e898183cea4fb31e00765effff981ca4c33a30df
An Phi <aphi@skidmore.edu>
2016-10-08 13:37:33 -0400

    USe API v1

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,4 +1,4 @@
-const API = '//localhost:2302/api';
+const API = '//localhost:2302/api/v1';
 export const BOOK_API = `${API}/books`;
 export const AUTH_API = `${API}/users`;
 export const LOGIN_API = `${AUTH_API}/login`;
