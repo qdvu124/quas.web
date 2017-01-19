c6880eece69e7cdc31197e56ff63e3d7bf26e13c
An Phi <aphi@skidmore.edu>
2017-01-19 09:55:11 -0500

    Update request header names

--- src/util/rest.js
+++ src/util/rest.js
@@ -4,8 +4,8 @@ export function getHeader() {
   return {
     'Content-Type': 'application/json',
     //TODO: There might be a safer way of doing this...
-    authorization: localStorage.getItem('token') == null ? '' : localStorage.getItem('token').replace('Bearer ', ''),
-    'x-language': '',
+    Authorization: localStorage.getItem('token') == null ? '' : localStorage.getItem('token').replace('Bearer ', ''),
+    'X-Language': '',
   };
 }
 
